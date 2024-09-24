8707ce0286f9f4361da0fd838dbfd118532e512f
jovanSimonovic <jsimonovic7021rn@raf.rs>
2024-09-24 16:47:51 +0200

    Added DI for ViewModel and some libs.

--- composeApp/src/commonMain/kotlin/org/cyb/opticmanager/App.kt
+++ composeApp/src/commonMain/kotlin/org/cyb/opticmanager/App.kt
@@ -4,9 +4,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import org.cyb.opticmanager.db.AppDatabase
 import org.koin.compose.KoinContext
-import org.koin.compose.koinInject
 
 @Composable
 @Preview
